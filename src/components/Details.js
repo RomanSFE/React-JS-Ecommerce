066790413aa666c686184e435b1b37ae13ab865b
Abdulla Al Roman <romannsu142@gmail.com>
2019-07-22 03:08:25 +0600

    Product show, details product show, ProductType create

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,12 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { ProductConsumer } from '../context';
+import {Link} from 'react-router-dom';
 
 class Details extends Component {
     render() {
         return (
-            <div>
-               <h3>Details...</h3> 
-            </div>
-        )
+            <ProductConsumer>
+                {value =>{
+                  const {id, img, price, company, title, inCart, info} = value.detailProduct;
+                  return (
+                      <div className="container py-4">
+                          {/* title part */}
+                          <div className="row">
+                              <div className="col-10 text-center title-style mx-auto my-4">
+                                  <h2>{title}</h2>
+                              </div>
+                          </div>
+                          {/* title part */}
+                          {/* product info */}
+                          <div className="row">
+                              <div className="col-10 mx-auto col-md-6 my-2 text-capitalize">
+                                    <img src={img} alt="product" className="img-fluid"/>
+                              </div>
+                              {/* product text */}
+                              <div className="col-10 mx-auto model-title col-md-6 my-2 text-capitalize">
+                                    <h2>model - {title}</h2>
+                                    <h4 className="company-title mt-3 mb-2 text-uppercase">
+                                        made by - <span className="com-maintitle">{company}</span>
+                                    </h4>
+                                    <h4 className="price-text"><strong>price - <span>$ </span>{price}</strong></h4>
+                                    <p className="text-capitalize font-weight-bold mt-3 mb-0">Info About The Product</p>
+                                    <p className="text-muted mt-1">{info}</p>
+                              </div>
+                          {/* product info */}
+                      </div>
+                    </div>
+                  );
+                }}
+            </ProductConsumer>
+        );
     }
 }
 export default Details
--- src/context.js
+++ src/context.js
@@ -5,9 +5,23 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state = {
-        products: storeProducts,
+        products: [],
         detailProduct: detailProduct
+    };
+    componentDidMount() {
+       this.setProducts(); 
     }
+    
+    setProducts = ()=>{
+        let tempProducts = [];
+        storeProducts.forEach(item =>{
+            const singleItem = {...item};
+            tempProducts = [...tempProducts, singleItem];
+        });
+        this.setState(() =>{
+           return {products: tempProducts};
+        });
+    };
     handleDetail = () =>{
         console.log("hello from detail")
     }
@@ -15,6 +29,7 @@ class ProductProvider extends Component {
     addtoCart = () =>{
         console.log("hello from Cart")
     }
+
     render() {
         return (
             <ProductContext.Provider value={{
